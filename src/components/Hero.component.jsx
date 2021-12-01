71b6a1e620755f2081024483f298292cf4ec5fa3
fulfordpr <fulfordpr.business@gmail.com>
2021-12-01 02:47:02 -0500

    update

--- src/components/Hero.component.jsx
+++ src/components/Hero.component.jsx
@@ -1,8 +1,14 @@
 import Collage from './Collage.component.jsx'
+import './Hero.styles.css'
 
 const Hero = () =>{
     return(
-        <Collage/>
+        <div className="container">
+            <Collage/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactiv activities led by one-of-a-kind host-all without leaving home.</p>
+        </div>
+        
     )
 }
 
