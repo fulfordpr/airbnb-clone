55926a542e51ceab05e7bec02972d90346db9255
Preston <fulfordpr.business@gmail.com>
2021-12-02 15:09:40 -0500

    added card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.component.jsx'
 import Hero from './components/Hero.component.jsx'
+import Card from './components/UI/Card.component.jsx'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Card/>
     </div>
   );
 }
--- src/components/Collage.component.jsx
+++ src/components/Collage.component.jsx
@@ -3,23 +3,23 @@ import './Collage.styles.css'
 const Collage = () =>{
     return(
         <section className="collage">
-            <div className="vertical">
+            <div className="vertical v1">
                 <img className="hero-img i9" src="https://images.unsplash.com/photo-1600210491892-03d54c0aaf87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
             </div>
-            <div className="vertical">
+            <div className="vertical v2">
                 <img className="hero-img i1" src="https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG9tZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
                 <img className="hero-img i2" src="https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aG9tZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
             </div>
-            <div className="vertical">
+            <div className="vertical v3">
                 <img className="hero-img i3" src="https://images.unsplash.com/photo-1502005229762-cf1b2da7c5d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
                 <img className="hero-img i4" src="https://images.unsplash.com/photo-1560440021-33f9b867899d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
             </div>
-            <div className="vertical">
+            <div className="vertical v4">
+                <img className="hero-img i7" src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80" alt="" />
                 <img className="hero-img i5" src="https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
                 <img className="hero-img i6" src="https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
             </div>
-            <div className="vertical">
-                <img className="hero-img i7" src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=958&q=80" alt="" />
+            <div className="vertical v5">
                 <img className="hero-img i8" src="https://images.unsplash.com/photo-1509660933844-6910e12765a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
             </div>
             
